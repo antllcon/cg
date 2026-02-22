b1231e863bb7d6eea07843d47415adb19c750c30
antlllcon <pokemonivan32@gmail.com>
2026-02-22 19:06:21 +0300

    [hangman] Добавил шрифт в приложение

--- src/system/AppConfig.h
+++ src/system/AppConfig.h
@@ -3,11 +3,13 @@
 
 namespace AppConfig
 {
-constexpr auto WINDOW_WIDTH = 400u;
-constexpr auto WINDOW_HEIGHT = 400u;
+constexpr auto WINDOW_WIDTH = 800u;
+constexpr auto WINDOW_HEIGHT = 600u;
 constexpr auto FRAMERATE_LIMIT = 60u;
-constexpr auto WINDOW_NAME = "Circle";
+constexpr auto WINDOW_NAME = "Hangman";
 constexpr auto BACKGROUND_COLOR = sf::Color::Black;
+constexpr auto FONT_PATH = "static/font.ttf";
+constexpr auto TOAST_FONT_SIZE = 12u;
 
 inline unsigned int GetMonitorRefreshRate()
 {
