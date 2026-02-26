ada9f86877353cc14e66b6c63d3b90529eb9d5f8
antlllcon <pokemonivan32@gmail.com>
2026-02-26 14:09:38 +0300

    [hangman] Исправил сцену приложения

--- src/scene/main/MainScene.cpp
+++ src/scene/main/MainScene.cpp
@@ -1,46 +1,51 @@
 #include "MainScene.h"
-#include "../../controller/circle/CircleController.h"
+#include "../../controller/game/KeyboardController.h"
 #include "../../controller/theme/ThemeController.h"
-#include "../../model/circle/CircleModel.h"
-#include "../../view/circle/CircleView.h"
+#include "../../model/game/KeyboardModel.h"
+#include "../../model/game/WordModel.h"
+#include "../../view/game/KeyboardView.h"
 #include "../../view/theme/ThemeView.h"
 #include "../../view/toast/ToastView.h"
 
 void MainScene::Init(std::shared_ptr<ThemeModel> themeModel)
 {
-	auto themeController = std::make_shared<ThemeController>(themeModel);
-	AddController(themeController);
+    auto themeController = std::make_shared<ThemeController>(themeModel);
+    AddController(themeController);
 
-	auto themeView = std::make_shared<ThemeView>(themeModel, themeController);
-	themeModel->RegisterObserver(themeView);
-	AddView(themeView);
+    auto themeView = std::make_shared<ThemeView>(themeModel, themeController);
+    themeModel->RegisterObserver(themeView);
+    AddView(themeView);
 
-	auto circleModel = std::make_shared<CircleModel>();
-	AddModel(circleModel);
+    auto toastModel = std::make_shared<ToastModel>();
+    AddModel(toastModel);
 
-	auto circleController = std::make_shared<CircleController>(circleModel);
-	AddController(circleController);
+    m_toastController = std::make_shared<ToastController>(toastModel);
+    AddController(m_toastController);
 
-	auto circleView = std::make_shared<CircleView>(circleModel, circleController);
-	circleModel->RegisterObserver(circleView);
-	AddView(circleView);
+    auto toastView = std::make_shared<ToastView>(toastModel, themeModel);
+    toastModel->RegisterObserver(toastView);
+    themeModel->RegisterObserver(toastView);
+    AddView(toastView);
 
-	auto toastModel = std::make_shared<ToastModel>();
-	AddModel(toastModel);
+    auto wordModel = std::make_shared<WordModel>();
+    AddModel(wordModel);
+    wordModel->SetNewRiddle({"MVC", "Паттерн проектирования", "Model-View-Controller"});
 
-	m_toastController = std::make_shared<ToastController>(toastModel);
-	AddController(m_toastController);
+    auto keyboardModel = std::make_shared<KeyboardModel>();
+    AddModel(keyboardModel);
 
-	auto toastView = std::make_shared<ToastView>(toastModel, themeModel);
-	toastModel->RegisterObserver(toastView);
-	themeModel->RegisterObserver(toastView);
-	AddView(toastView);
+    auto keyboardController = std::make_shared<KeyboardController>(keyboardModel, wordModel);
+    AddController(keyboardController);
+
+	auto keyboardView = std::make_shared<KeyboardView>(keyboardController, keyboardModel->GetData());
+    keyboardModel->RegisterObserver(keyboardView);
+    AddView(keyboardView);
 }
 
 void MainScene::OnException(const std::exception& e)
 {
-	if (m_toastController)
-	{
-		m_toastController->ShowError(e.what());
-	}
+    if (m_toastController)
+    {
+        m_toastController->ShowError(e.what());
+    }
 }
