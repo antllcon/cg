ec0c75914f309fdfa957dd24a39bd5a0b8f49d18
antlllcon <pokemonivan32@gmail.com>
2026-03-20 15:50:20 +0300

    [bezier-curve] Исправил тип данных для точки

--- src/core/types/Point.h
+++ src/core/types/Point.h
@@ -1,29 +1,7 @@
 #pragma once
+#include <libs/glm/glm.hpp>
 
-template <typename T>
-struct Point
-{
-	T x{0};
-	T y{0};
-
-	bool operator==(const Point&) const = default;
-
-	template <typename U>
-	Point<U> As() const
-	{
-		return Point<U>{static_cast<U>(x), static_cast<U>(y)};
-	}
-
-	Point operator+(const Point& other) const
-	{
-		return Point{x + other.x, y + other.y};
-	}
-
-	Point operator-(const Point& other) const
-	{
-		return Point{x - other.x, y - other.y};
-	}
-};
-
-using Point2i = Point<int>;
-using Point2f = Point<float>;
+using Point2i = glm::ivec2;
+using Point2f = glm::vec2;
+using Point3f = glm::vec3;
+using Point4f = glm::vec4;
