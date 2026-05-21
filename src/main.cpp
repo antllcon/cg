4d82dcffa2a39d5e0fc58dca6b9c9588445f6514
antlllcon <pokemonivan32@gmail.com>
2026-05-22 00:38:03 +0300

    [median] Исправил корень программы

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,20 @@
 #include "Windows.h"
 #include "system/application/Application.h"
-#include "system/managers/AudioManager.h"
-#include "system/render/opengl/OpenglRenderer.h"
-#include "system/window/GlfwWindow.h"
+#include "utils/console/ConsoleEncoding.h"
 #include <iostream>
-#include <memory>
 
 int main()
 {
-	SetConsoleOutputCP(CP_UTF8);
-	SetConsoleCP(CP_UTF8);
+	ConsoleEncoding encoding;
 
 	try
 	{
-		auto window = std::make_unique<GlfwWindow>();
-		auto renderer = std::make_unique<OpenglRenderer>();
-		auto audio = std::make_unique<AudioManager>();
-
-		auto app = std::make_shared<Application>(std::move(window), std::move(renderer), std::move(audio));
-
-		app->Init();
-		app->Run();
+		auto app = Application();
+		app.Run();
 	}
 	catch (const std::exception& e)
 	{
-		std::cerr << "Критическая ошибка: " << e.what() << std::endl;
-		return EXIT_FAILURE;
+		std::cerr << "[Error] \t" << e.what() << std::endl;
 	}
 
 	return EXIT_SUCCESS;
