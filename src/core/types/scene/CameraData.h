91aac6810d91f3e8b44aa5e6adb16bcf1c4ea1c2
antlllcon <pokemonivan32@gmail.com>
2026-05-21 17:09:12 +0300

    [median] Почистил код от мусора

--- src/core/types/scene/CameraData.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-struct CameraData
-{
-	Mat4 viewMatrix{1.0f};
-	Mat4 projectionMatrix{1.0f};
-	Point3f position{0.0f, 0.0f, 0.0f};
-};
--- src/core/utils/FileReader.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <filesystem>
-#include <string>
-
-namespace FileReader
-{
-std::string ReadFileToString(const std::filesystem::path& path);
-}
