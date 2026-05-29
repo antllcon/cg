2506686238a260c6fe7ee769fb2a19b2f679f01e
antlllcon <pokemonivan32@gmail.com>
2026-05-30 00:10:33 +0300

    [median] Подправил реализацию для обхода оптимизации на Windows

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,12 @@
 #include "utils/console/ConsoleEncoding.h"
 #include <iostream>
 
+#ifdef _WIN32
 extern "C" {
 __declspec(dllexport) uint32_t NvOptimusEnablement = 1;
 __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
 }
+#endif
 
 int main()
 {
@@ -14,12 +16,13 @@ int main()
 
 	try
 	{
-		auto app = Application();
+		Application app;
 		app.Run();
 	}
 	catch (const std::exception& e)
 	{
 		std::cerr << "[Error] \t" << e.what() << std::endl;
+		return EXIT_FAILURE;
 	}
 
 	return EXIT_SUCCESS;
